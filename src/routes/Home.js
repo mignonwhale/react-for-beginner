7b01f4890c3c2b720d06cd281435bfb3573f3c94
mignonwhale <mignonwhale@gmail.com>
2025-05-14 18:21:33 +0900

    add sort

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -11,26 +11,51 @@ function Home() {
     setMovies(json.data.movies);
     setLoading(false);
   };
+  const sortMovies = (sortBy) => {
+    const sortedMovies = movies;
+    return sortedMovies.slice().sort((a, b) => {
+      if (sortBy === "rating") {
+        return b.rating - a.rating; // 높은 rating이 먼저
+      } else {
+        return new Date(b.date_uploaded) - new Date(a.date_uploaded); // 최신 업로드가 먼저
+      }
+    });
+  };
+
+  const handleSoring = (event) => {
+    const sortedMovies = sortMovies(event.target.value);
+    console.log(sortedMovies);
+    setMovies(sortedMovies);
+  };
+
   useEffect(() => {
     getMoveis();
   }, []);
 
   return (
     <div>
-      <h1>Movie App</h1>
       {loading ? (
         <div>Loading...</div>
       ) : (
-        <div className={styles.movieGrid}>
-          {movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              id={movie.id}
-              coverImg={movie.medium_cover_image}
-              title={movie.title}
-              rating={movie.rating}
-            />
-          ))}
+        <div className={styles.container}>
+          <div className={styles.sort}>
+            <select onChange={handleSoring} className={styles.selector}>
+              <option value="">sort by</option>
+              <option value="rating">rating</option>
+              <option value="date">uploadedDate</option>
+            </select>
+          </div>
+          <div className={styles.movieGrid}>
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                coverImg={movie.medium_cover_image}
+                title={movie.title}
+                rating={movie.rating}
+              />
+            ))}
+          </div>
         </div>
       )}
     </div>
